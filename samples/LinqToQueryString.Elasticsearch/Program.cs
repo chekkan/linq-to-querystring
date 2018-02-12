c9e016a6d0b99c4d61d77883b5ff458a9651d3f8
Harish Babu <harish.babu@creditsafe.com>
2018-02-12 14:27:09 +0000

    added linq to query string elasticsearch sample console app

---
+++ samples/LinqToQueryString.Elasticsearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqToQueryString.Elasticsearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
